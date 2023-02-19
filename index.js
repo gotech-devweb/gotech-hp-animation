8fe48932c6ba08aceb4a7c8c00aef0a4df1620a9
Carson <trongld21@gmail.com>
2023-02-19 10:16:30 +0700

    responsive UI  gotech hp

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $('.owl-carousel').owlCarousel({
     autoplay: true, // Tự động chuyển slide
-    dots: true, // Hiển thị chấm chỉ số slide
-    nav: true, // Hiển thị nút điều khiển trái/phải
+    dots: false, // Hiển thị chấm chỉ số slide
+    nav: false, // Hiển thị nút điều khiển trái/phải
     loop: true, // Cho phép lặp lại slide
     speed: 500, // Tốc độ chuyển đổi slide
     items: 1, // Số slide hiển thị trên màn hình
@@ -10,16 +10,16 @@ $('.owl-carousel').owlCarousel({
     responsive: {
         0: {
             items: 1,
-            nav: true
+            nav: false
         },
         600: {
             items: 1,
             nav: false
         },
-        1000: {
-            items: 2,
-            nav: true,
-            loop: false
+        768: {
+            items: 1,
+            nav: false,
+            loop: true
         }
     }
 });
